89c5c464b7300c30b005ec41755a93ac2a7895e0
Danjuma1 <hda0202@gmail.com>
2022-10-19 21:33:59 +0100

    changed database settings to mysql

--- greenmark/greenmark/settings.py
+++ greenmark/greenmark/settings.py
@@ -74,10 +74,21 @@ WSGI_APPLICATION = 'greenmark.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE'  : 'django.db.backends.mysql', # <-- UPDATED line 
+        'NAME'    : 'mvc',                      # <-- UPDATED line 
+        'USER'    : 'mvc',                     # <-- UPDATED line
+        'PASSWORD': 'Communi_cate',              # <-- UPDATED line
+        'HOST'    : 'localhost',                # <-- UPDATED line
+        'PORT'    : '3306',
     }
 }
 
