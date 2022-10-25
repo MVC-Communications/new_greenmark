49481bec2832e0f6b3703da6834479cf48fdc7b6
Danjuma1 <hda0202@gmail.com>
2022-10-25 09:55:35 +0100

    removed redundant forms

--- greenmark/main/forms.py
+++ greenmark/main/forms.py
@@ -4,37 +4,11 @@ from django.forms import ModelForm
 
 from .models import *
 
-class AgreementDocForm(ModelForm):
-    class Meta:
-        model = AgreementDoc
-        fields = '__all__'
-
 class BussInfoForm(ModelForm):
     class Meta:
         model = BussInfo
-        fields = '__all__'
-
-class KeyDecisionMakerForm(ModelForm):
-    class Meta:
-        model = KeyDecisionMaker
-        fields = '__all__'
-
-class EnvironManagerForm(ModelForm):
-     class Meta:
-        model = EnvironManager
-        fields = '__all__'
-
-class OtherResponsiblePersonForm(ModelForm):
-    class Meta:
-        model = OtherResponsiblePerson
-        fields = '__all__'
-
+        exclude = ['user']
 class EnvCompObligForm(ModelForm):
     class Meta:
         model = EnvCompOblig
-        fields = '__all__'
-
-class EnvCommForm(ModelForm):
-    class Meta:
-        model = EnvComm
         fields = '__all__'
