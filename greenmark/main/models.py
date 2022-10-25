de77730bd56e5d24ed11b44ed639a85ff1d564f2
Danjuma1 <hda0202@gmail.com>
2022-10-25 09:54:34 +0100

    rebuilt the models

--- greenmark/main/models.py
+++ greenmark/main/models.py
@@ -1,5 +1,6 @@
 from random import choices
 from django.db import models
+from django.contrib.auth.models import User
 
 
 ENVIRON_COMM_CHOICES = (
@@ -15,16 +16,9 @@ IMPACTS_CHOICES = (
     ('Water','Water')
 )
 
-class AgreementDoc(models.Model):
-    # doc_id = models.BigAutoField(primary_key=True)
-    year_made = models.DateField()
-
-    def __str__(self):
-        pass 
-
 class BussInfo(models.Model):
-    # doc_id = models.ForeignKey(AgreementDoc, on_delete=models.CASCADE)
-    # buss_id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    year_of_aggrement = models.DateField()
     buss_name = models.CharField(max_length=50)
     # buss_logo = models.ImageField(upload_to='media/BusinessLogos')
     phone = models.CharField(max_length=15)
@@ -33,48 +27,31 @@ class BussInfo(models.Model):
     location2 = models.CharField(max_length=70, blank=True)
     location3 = models.CharField(max_length=70, blank=True)
     location4 = models.CharField(max_length=70, blank=True)
+    keydecider_name = models.CharField(max_length=30)
+    keydecider_title = models.CharField(max_length=30)
+    keydecider_email = models.CharField(max_length=30)
+    keydecider_signature = models.CharField(max_length=30)
+    envmanager_name = models.CharField(max_length=30)
+    envmanager_title = models.CharField(max_length=30)
+    envmanager_email = models.CharField(max_length=30)
+    envmanager_signature = models.CharField(max_length=30)
+    otherresp_person_name = models.CharField(max_length=30, blank=True)
+    otherresp_person_title = models.CharField(max_length=30, blank=True)
+    otherresp_person_email = models.CharField(max_length=30, blank=True)
+    otherresp_person_signature = models.CharField(max_length=30, blank=True)
+    
 
     def __str__(self):
         pass 
 
-class KeyDecisionMaker(models.Model):
-    # doc_id = models.ForeignKey(AgreementDoc, on_delete=models.CASCADE)
-    # buss_id = models.ForeignKey(BussInfo, on_delete=models.CASCADE)
-    name = models.CharField(max_length=30)
-    title = models.CharField(max_length=30)
-    email = models.CharField(max_length=30)
-    signature = models.CharField(max_length=30)
 
-class EnvironManager(models.Model):
-    # doc_id = models.ForeignKey(AgreementDoc, on_delete=models.CASCADE)
-    # buss_id = models.ForeignKey(BussInfo, on_delete=models.CASCADE)
-    name = models.CharField(max_length=30)
-    title = models.CharField(max_length=30)
-    email = models.CharField(max_length=30)
-    signature = models.CharField(max_length=30)
-
-class OtherResponsiblePerson(models.Model):
-    # doc_id = models.ForeignKey(AgreementDoc, on_delete=models.CASCADE)
-    # buss_id = models.ForeignKey(BussInfo, on_delete=models.CASCADE)
-    name = models.CharField(max_length=30)
-    title = models.CharField(max_length=30)
-    email = models.CharField(max_length=30)
-    signature = models.CharField(max_length=30)
 
 class EnvCompOblig(models.Model):
-    # doc_id = models.ForeignKey(AgreementDoc, on_delete=models.CASCADE)
-    # buss_id = models.ForeignKey(BussInfo, on_delete=models.CASCADE)
-    agreement_name = models.CharField(max_length=50)
-    description = models.TextField()
-
-    def __str__(self):
-        pass 
-
-class EnvComm(models.Model):
-    # doc_id = models.ForeignKey(AgreementDoc, on_delete=models.CASCADE)
-    # buss_id = models.ForeignKey(BussInfo, on_delete=models.CASCADE)
-    comm_option = models.CharField(max_length=50, choices=ENVIRON_COMM_CHOICES)
-    description = models.TextField()
+    document = models.ForeignKey(BussInfo, on_delete=models.CASCADE)
+    comp_oblig_name = models.CharField(max_length=50)
+    comp_oblig_description = models.TextField()
+    env_comm_option = models.CharField(max_length=50, choices=ENVIRON_COMM_CHOICES)
+    env_comm_description = models.TextField()
 
     def __str__(self):
         pass 
