37591d77b7edc0a290aee83be46f609efff68719
Danjuma1 <hda0202@gmail.com>
2022-11-07 16:50:42 +0100

    added more models but yet to migrate them to db

--- greenmark/main/models.py
+++ greenmark/main/models.py
@@ -16,6 +16,18 @@ IMPACTS_CHOICES = (
     ('Water','Water')
 )
 
+PROBABILITY_CHOICES = (
+    ('High','High'),
+    ('Medium','Medium'),
+    ('Low','Low')
+)
+
+IF_OCCURS_CHOICES = (
+    ('Major','Major'),
+    ('Medium','Medium'),
+    ('Minor','Minor')
+)
+
 class BussInfo(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     year_of_aggrement = models.DateField()
@@ -40,12 +52,11 @@ class BussInfo(models.Model):
     otherresp_person_email = models.CharField(max_length=30, blank=True)
     otherresp_person_signature = models.CharField(max_length=30, blank=True)
     
-
     def __str__(self):
         pass 
 
 class EnvCompOblig(models.Model):
-    document = models.ForeignKey(BussInfo, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     comp_oblig_name = models.CharField(max_length=50)
     comp_oblig_description = models.TextField()
     env_comm_option = models.CharField(max_length=50, choices=ENVIRON_COMM_CHOICES)
@@ -56,18 +67,56 @@ class EnvCompOblig(models.Model):
 
 class AspImpact(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    activity_1 = models.CharField(max_length=50)
-    aspect_1 = models.CharField(max_length=50)
-    impact_1 = models.CharField(max_length=50)
-    activity_2 = models.CharField(max_length=50)
-    aspect_2 = models.CharField(max_length=50)
-    impact_2 = models.CharField(max_length=50)
-    activity_3 = models.CharField(max_length=50)
-    aspect_3 = models.CharField(max_length=50)
-    impact_3 = models.CharField(max_length=50)
-    activity_4 = models.CharField(max_length=50)
-    aspect_4 = models.CharField(max_length=50)
-    impact_4 = models.CharField(max_length=50)
+    activity_1 = models.CharField(max_length=50, blank=True)
+    aspect_1 = models.CharField(max_length=50, blank=True)
+    impact_1 = models.CharField(max_length=50, choices=IMPACTS_CHOICES, blank=True)
+    activity_2 = models.CharField(max_length=50, blank=True)
+    aspect_2 = models.CharField(max_length=50, blank=True)
+    impact_2 = models.CharField(max_length=50, choices=IMPACTS_CHOICES, blank=True)
+    activity_3 = models.CharField(max_length=50, blank=True)
+    aspect_3 = models.CharField(max_length=50, blank=True)
+    impact_3 = models.CharField(max_length=50, choices=IMPACTS_CHOICES, blank=True)
+    activity_4 = models.CharField(max_length=50, blank=True)
+    aspect_4 = models.CharField(max_length=50, blank=True)
+    impact_4 = models.CharField(max_length=50, choices=IMPACTS_CHOICES, blank=True)
+    activity_5 = models.CharField(max_length=50, blank=True)
+    aspect_5 = models.CharField(max_length=50, blank=True)
+    impact_5 = models.CharField(max_length=50, choices=IMPACTS_CHOICES, blank=True)
+    activity_6 = models.CharField(max_length=50, blank=True)
+    aspect_6 = models.CharField(max_length=50, blank=True)
+    impact_6 = models.CharField(max_length=50, choices=IMPACTS_CHOICES, blank=True)
+    activity_7 = models.CharField(max_length=50, blank=True)
+    aspect_7 = models.CharField(max_length=50, blank=True)
+    impact_7 = models.CharField(max_length=50, choices=IMPACTS_CHOICES, blank=True)
+    activity_8 = models.CharField(max_length=50, blank=True)
+    aspect_8 = models.CharField(max_length=50, blank=True)
+    impact_8 = models.CharField(max_length=50, choices=IMPACTS_CHOICES, blank=True)
+    activity_9 = models.CharField(max_length=50, blank=True)
+    aspect_9 = models.CharField(max_length=50, blank=True)
+    impact_9 = models.CharField(max_length=50, choices=IMPACTS_CHOICES, blank=True)
+    activity_10 = models.CharField(max_length=50, blank=True)
+    aspect_10 = models.CharField(max_length=50, blank=True)
+    impact_10 = models.CharField(max_length=50, choices=IMPACTS_CHOICES, blank=True)
 
     def __str__(self):
         pass 
+
+class EnvThreats(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    threat = models.CharField(max_length=50)
+    impact = models.CharField(max_length=20, choices=IMPACTS_CHOICES)
+    probability = models.CharField(max_length=20, choices=PROBABILITY_CHOICES)
+    if_occurs = models.CharField(max_length=20, choices=IF_OCCURS_CHOICES)
+
+    def __str__(self):
+        pass 
+
+class Trainers(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    trainer_name = models.CharField(max_length=50)
+    trainer_email = models.CharField(max_length=50)
+    trainee_name = models.CharField(max_length=50)
+    trainee_email = models.CharField(max_length=50)
+
+    def __str__(self):
+        return 
