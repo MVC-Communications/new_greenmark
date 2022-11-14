25bb6fdbf58b4b4cb374d2f69c1ad125632a1a7f
Danjuma1 <hda0202@gmail.com>
2022-11-14 17:46:08 +0100

    created new urls

--- greenmark/main/urls.py
+++ greenmark/main/urls.py
@@ -5,6 +5,8 @@ app_name = "main"
 
 
 urlpatterns = [
+    path("iso_start", views.iso_start, name="iso_start"),
     path("iso_step_one", views.iso_step_one, name="iso_one"),
+    path("iso_buss_info", views.iso_buss_info, name="iso_buss_info"),
     path("print_doc", views.print_doc, name="print_doc"),
 ]
