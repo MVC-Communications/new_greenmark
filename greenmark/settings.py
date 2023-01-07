9737fb1d9e4da3cf8fde236a937b2b888f03e11f
hudeh <hudeh30@gmail.com>
2023-01-07 13:58:28 +0100

    setup

--- greenmark/settings.py
+++ greenmark/settings.py
@@ -12,6 +12,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
+# SECRET_KEY = "config('SECRET_KEY')"
+# DEBUG = True
 ALLOWED_HOSTS = ["*"]
 
 
@@ -62,14 +64,15 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'greenmark.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
 # DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql_psycopg2",
+#         "NAME": "greenmark",
+#         "USER": "postgres",
+#         "PASSWORD": "postgres",
+#         "HOST": "localhost",
+#         "PORT": "5432",
 #     }
 # }
 
